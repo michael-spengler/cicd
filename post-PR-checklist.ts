38a6554c22ec6b80ccc9627249812c3c10eaa24e
michael-spengler <mycamper@gmx.de>
2020-10-19 00:40:07 +0200

    logging

--- post-PR-checklist.ts
+++ post-PR-checklist.ts
@@ -4,8 +4,12 @@ import { defaultOptions } from 'https://deno.land/x/request@1.3.0/interfaces-and
 export async function postPRChecklist() {
 
     const repo = Deno.args[1]
-    const issueNumber = Deno.args[2]
-    const url = `https://api.github.com/repos/${repo}/issues/${issueNumber}/comments`
+    console.log(`repo: ${repo}`)
+
+    const prNumber = Deno.args[2]
+    console.log(`prNumber: ${prNumber}`)
+
+    const url = `https://api.github.com/repos/${repo}/issues/${prNumber}/comments`
 
     const commentContent = {
         "body": `${await getContent(repo, 'pr-checklist.md')}`
