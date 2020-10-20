1e10d1360c612da04a7cb38e07d6beaf3d8fb86f
michael-spengler <mycamper@gmx.de>
2020-10-21 00:02:46 +0200

    release workflow

--- create-minor-release.ts
+++ create-minor-release.ts
@@ -1,8 +1,47 @@
 
-export function createMinorRelease () {
+import { Request } from 'https://deno.land/x/request@1.3.0/request.ts'
+import { defaultOptions } from 'https://deno.land/x/request@1.3.0/interfaces-and-constants.ts'
+import "https://deno.land/x/dotenv/load.ts"
+
+import { Helper, ESemanticVersion } from './helper.ts'
+
+
+export async function createMinorRelease() {
+
+    const token = Deno.env.get('token') as string
+
+    defaultOptions.headers = {
+        'Authorization': `token ${token}`
+    }
+
+    const repo = Deno.args[1] as string
+
+    const nextVersion = await getNextVersion(token, repo)
     
-    // tbd 
-    throw new Error('not yet implemented')
+    console.log(nextVersion)
+
+    const url = `https://api.github.com/repos/${Deno.args[1]}/releases`
+
+    const xy = {
+        "tag_name": nextVersion,
+        "target_commitish": "main",
+        "name": nextVersion,
+        "body": "Description of the release",
+        "draft": false,
+        "prerelease": false
+    }
+
+
+    await Request.post(url, xy)
+
+}
+
+async function getNextVersion(token: string, repo: string): Promise<string> {
+    const url = `https://api.github.com/repos/${repo}/releases/latest`
+
+    const result = await Request.get(url)
+
+    return Helper.incrementVersion(result.tag_name, ESemanticVersion.MINOR)
 }
 
 createMinorRelease()
---
+++ helper.ts
@@ -0,0 +1,29 @@
+
+export enum ESemanticVersion {
+    PATCH = 1,
+    MINOR,
+    MAJOR
+}
+
+export class Helper {
+
+    public static incrementVersion(currentVersion: string, semanticVersion: ESemanticVersion): string {
+
+        let major = Number(currentVersion.split('v')[1].split('.')[0])
+        let minor = Number(currentVersion.split('.')[1].split('.')[0])
+        let patch = Number(currentVersion.split('.')[2])
+
+        if (semanticVersion === ESemanticVersion.PATCH) {
+            patch = patch + 1
+        } else if (semanticVersion === ESemanticVersion.MINOR){
+            minor = minor + 1
+            patch = 0
+        } else if (semanticVersion === ESemanticVersion.MAJOR){
+            major = major + 1
+            minor = 0
+            patch = 0
+        }
+
+        return `v${major}.${minor}.${patch}`
+    }
+}
