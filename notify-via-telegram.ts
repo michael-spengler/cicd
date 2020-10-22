49af012c6e2d444ee66eae470e135288df53e0d8
michael-spengler <mycamper@gmx.de>
2020-10-22 23:41:40 +0200

    notify via telegram

--- notify-via-telegram.ts
+++ notify-via-telegram.ts
@@ -1,8 +1,17 @@
+import { Request } from 'https://deno.land/x/request@1.3.0/request.ts'
 
-export function notifyViaTelegram() {
+export async function notifyViaTelegram() {
     
-        // tbd - e.g. using https://nest.land/package/telegram-bot-api
-    throw new Error('not yet implemented')
+    const telegramBotToken = Deno.args[0]
+    const telegramTargetChatId = Deno.args[1]
+    const repo = Deno.args[2]
+    const message = Deno.args[3]
+    
+    const messageToBeSent = `https://github.com/${repo}: ${message}`
+
+    const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${telegramTargetChatId}&text=${messageToBeSent}&disable_web_page_preview=true`
+    
+    await Request.get(url)
 }
 
 notifyViaTelegram()
