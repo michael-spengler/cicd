b14e88ce5d4730e4884ac8f890eded60a48fca2d
michael-spengler <mycamper@gmx.de>
2020-10-24 01:20:27 +0200

    just to be complete :)

--- mod.ts
+++ mod.ts
@@ -1,17 +1,18 @@
-export * from './create'
+
 export * from './create-major-release.ts'
 export * from './create-minor-release.ts'
 export * from './create-patch-release.ts'
 export * from './helper.ts'
 export * from './lint.ts'
+export * from './notify-via-discord.ts'
+export * from './notify-via-ms-teams.ts'
+export * from './notify-via-slack.ts'
+export * from './notify-via-telegram.ts'
 export * from './post-PR-checklist.ts'
 export * from './publish-on-nest.land.ts'
 export * from './README.md'
 export * from './request-cla-signature.ts'
 export * from './request-dco-signature.ts'
 export * from './security-scan.ts'
-export * from './lint.ts'
-export * from './notify-via-discord.ts'
-export * from './notify-via-telegram.ts'
-export * from './notify-via-slack.ts'
+
 
