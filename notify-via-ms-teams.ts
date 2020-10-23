fca7ac1e0c3801f08fb71fa468ff928a1447fbb1
michael-spengler <mycamper@gmx.de>
2020-10-23 23:34:39 +0200

    notify via ms teams - create patch release

---
+++ notify-via-ms-teams.ts
@@ -0,0 +1,10 @@
+import { Request } from 'https://deno.land/x/request@1.3.0/request.ts'
+
+export async function notifyViaMSTeams() {
+
+// https://docs.microsoft.com/en-us/graph/teams-concept-overview 
+// https://docs.microsoft.com/en-us/graph/api/resources/teams-api-overview?view=graph-rest-1.0
+
+}
+
+notifyViaMSTeams()
