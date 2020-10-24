bd43a66b6f1e04fde87587a3489beadfc9b07cc7
michael-spengler <mycamper@gmx.de>
2020-10-25 01:35:22 +0200

    notify via discord - create minor release

--- notify-via-discord.ts
+++ notify-via-discord.ts
@@ -1,8 +1,26 @@
 
-export function notifyViaDiscord() {
-    
-        // tbd - e.g. using https://nest.land/package/Discordeno
-    throw new Error('not yet implemented')
+import { Request } from 'https://deno.land/x/request@v1.3.1/request.ts'
+
+// import "https://deno.land/x/dotenv/load.ts"
+// const discordWebHookUrl = Deno.env.get('discordWebHook') as string
+
+export async function notifyViaDiscord() {
+
+    const discordWebHookUrl = Deno.args[0]
+    const repo = Deno.args[1]
+    const message = Deno.args[2]
+
+    const payload = {
+        "content": `https://github.com/${repo}: ${message}`
+    };
+
+    try {
+        await Request.post(discordWebHookUrl, payload)
+    } catch (error) {
+        console.log(error.message)
+    }
+
 }
 
 notifyViaDiscord()
+
