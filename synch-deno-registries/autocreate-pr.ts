8c36f52597e1880a4ab75c1da50ccfff3c77e17f
michael-spengler <mycamper@gmx.de>
2020-10-24 22:32:55 +0200

    preparing the regular sync between different deno module registries

---
+++ synch-deno-registries/autocreate-pr.ts
@@ -0,0 +1,18 @@
+export async function autocreatePR() {
+    console.log('get the party started')
+    // 1. read from modules-to-be-prepared-for-nest.land.ts
+
+    const modulesToBePreparedForNestDotLand = []
+
+    for (const module of modulesToBePreparedForNestDotLand) {
+        
+    }
+
+}
+
+async function createPR() {
+ // https://developer.github.com/v3/pulls/#create-a-pull-request
+
+}
+
+autocreatePR()
---
+++ synch-deno-registries/modules-to-be-prepared-for-nest.land.ts
@@ -0,0 +1,7 @@
+[
+    {
+        name: "countries",
+        repository: "https://github.com/michael-spengler/countries",
+        description: "🦕 module which provides information about countries on earth + mars"
+    }
+]
